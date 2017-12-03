a9db36dd5df49bbdf23f431bef18a0e3f42d5d92
ier005 <ier005@users.noreply.github.com>
2017-12-03 16:13:49 +0800

    test

--- enccom/packet_handle.c
+++ enccom/packet_handle.c
@@ -32,14 +32,14 @@ int handle_packet_out(struct sk_buff *skb)
 		if (opt->ip == ipheader->daddr) {
 			unsigned char *buf = skb_network_header(skb) + ipheader->ihl * 4;
 			printk("dport: %hu\n", ntohs(*(short *)(buf+2)));
-			
+			/*
 			struct tcphdr *tcph = tcp_hdr(skb);
 			tcph->check = 0;
 			skb->csum = csum_partial((unsigned char *)tcph, ntohs(ipheader->tot_len) - ip_hdrlen(skb), 0);
 			tcph->check = csum_tcpudp_magic(ipheader->saddr, ipheader->daddr, ntohs(ipheader->tot_len) - ip_hdrlen(skb), ipheader->protocol, skb->csum);
 			ipheader->check = 0;
 			ipheader->check = ip_fast_csum(ipheader, ipheader->ihl);
-			skb->ip_summed = CHECKSUM_NONE;
+			skb->ip_summed = CHECKSUM_NONE;*/
 			crypt(ENCCOM_ENCRYPT, buf, skb_tail_pointer(skb) - buf, opt->key);
 			break;
 		}
